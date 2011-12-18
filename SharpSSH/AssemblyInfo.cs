141e64d0cbd05c81ba35dbb572ced7eef33302cb
TaoK <tao@klerks.biz>
2011-12-18 09:14:52 -0500

    Increment version number, change copyright notice to help differenciate binary

--- SharpSSH/AssemblyInfo.cs
+++ SharpSSH/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 //
 // General Information about an assembly is controlled through the following 
@@ -9,9 +10,9 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("SharpSSH")]
 [assembly: AssemblyDescription("SSH library for .NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("tamirgal.com")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SharpSSH")]
-[assembly: AssemblyCopyright("Tamir Gal (c) 2007 and jcraft.com")]
+[assembly: AssemblyCopyright("Tamir Gal (c) 2007 and jcraft.com (modified by Tao Klerks, 2011)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
@@ -26,7 +27,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.1.1.14")]
+[assembly: AssemblyVersion("1.1.2.1")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
@@ -56,3 +57,4 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
+[assembly: ComVisibleAttribute(false)]
